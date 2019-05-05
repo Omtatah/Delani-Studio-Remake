eb92594a470e8e8e645d8ba6ab55766744abee69
Omtatah <murtallahomtatah@gmail.com>
2019-05-05 21:13:14 +0300

    style every div with text with appropriate black and white colors

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('#ldI').click(function () {
+        $('#show1').show;
+        $('#hide1').hide;
+    });
+    $('#show1').click(function () {
+        $('#hide1').show;
+        $('#show1').hide;
+    });
+    $('#hide2').click(function () {
+        $('#show2').show;
+        $('#hide2').hide;
+    });
+    $('#show2').click(function () {
+        $('#hide2').show;
+        $('#show2').hide;
+    });
+    $('#hide3').click(function () {
+        $('#show3').show;
+        $('#hide3').hide;
+    }); 
+    $('#show3').click(function () {
+        $('#hide3').show;
+        $('#show3').hide;
+    });
+});
